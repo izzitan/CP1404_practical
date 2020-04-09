541ed101a8cc0f68a6067edbf89b90c1cf4db839
Izzi <izzi.tan2@gmail.com>
2020-04-09 20:54:56 +0800

    Update quick picks

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -4,25 +4,28 @@ GitHub: https://github.com/izzitan/CP1404_practical
 """
 import random
 
-CONSTANTS = []
+NUMBERS = []
+NUMBERS_OF_LINE = 6
+MINIMUM = 1
+MAXIMUM = 45
 
 
 def main():
     quick_pick = int((input("How many quick picks? ")))
     for line in range(quick_pick):
         random_numbers = []
-        for i in range(6):
+        for i in range(NUMBERS_OF_LINE):
             random_numbers.append(generate_random_number())
         print(sorted(random_numbers))
 
 
 def generate_random_number():
     while True:
-        random_number = random.randint(1, 45)
-        if random_number in CONSTANTS:
+        random_number = random.randint(MINIMUM, MAXIMUM)
+        if random_number in NUMBERS:
             pass
         else:
-            CONSTANTS.append(random_number)
+            NUMBERS.append(random_number)
             return random_number
 
 
