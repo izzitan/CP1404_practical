d0687a928da7767aa7a8a733a13a458cd59e8367
Izzi <izzi.tan2@gmail.com>
2020-04-16 17:19:55 +0800

    Update state names

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -7,21 +7,25 @@ File needs reformatting
 """
 
 CODE_TO_NAME = {
-                "QLD": "Queensland",
-                "NSW": "New South Wales",
-                "NT": "Northern Territory",
-                "WA": "Western Australia",
-                "ACT": "Australian Capital Territory",
-                "VIC": "Victoria",
-                "TAS": "Tasmania"
-                }
+    "QLD": "Queensland",
+    "NSW": "New South Wales",
+    "NT": "Northern Territory",
+    "WA": "Western Australia",
+    "ACT": "Australian Capital Territory",
+    "VIC": "Victoria",
+    "TAS": "Tasmania"
+}
 
 print(CODE_TO_NAME)
 
 state_code = input("Enter short state: ")
 while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
+    if state_code.upper() in CODE_TO_NAME:
+        print(state_code.upper(), "is", CODE_TO_NAME[state_code.upper()])
     else:
         print("Invalid short state")
     state_code = input("Enter short state: ")
+
+for code in CODE_TO_NAME:
+    print("""{}  is  {}""".format(code,CODE_TO_NAME[code]))
+
