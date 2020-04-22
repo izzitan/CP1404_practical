06f993565a677a6ed2414fa4cc736d2958d718da
Izzi <izzi.tan2@gmail.com>
2020-04-22 17:52:26 +0800

    Update languages

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -3,13 +3,20 @@ Name:   Hafidz Izzi Baihaqi
 GitHub: https://github.com/izzitan/CP1404_practical
 """
 
+
 class ProgrammingLanguage:
 
-    def __init__(self, name="name", typing="type", reflection=False, year=0):
+    def __init__(self, name="", typing="", reflection=False, year=0):
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
-        return True if self.typing == "Dynamic" else False
+        if self.typing == "Dynamic":
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name, self.typing, self.reflection, self.year)
