1619a8c100ff0c3b22a7a53a5573f1e967beb645
Izzi <izzi.tan2@gmail.com>
2020-05-14 18:15:10 +0800

    Add taxi and taxi test

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,18 @@
+"""
+Name:   Hafidz Izzi Baihaqi
+GitHub: https://github.com/izzitan/CP1404_practical
+"""
+
+from prac_08.taxi import Taxi
+
+def main():
+    taxi = Taxi("Prius 1", 100, 1.23)
+    taxi.drive(40)
+    print(taxi)
+    taxi.start_fare()
+    taxi.drive(100)
+    print(taxi)
+    print("Total fare is: ${:.2f}".format(taxi.get_fare()))
+
+
+main()
